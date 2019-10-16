879ad06bdad353f82c6021b02e06ce11db50a75d
Gaje Robarge <gajer@paulmitchell.edu>
2019-10-16 11:27:54 -0600

    have signup page done

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 import Map from './Components/Map';
-import LoginSignup from './Components/Login-Signup';
+import LoginScreen from './Components/LoginScreen';
 
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 const MainNavigator = createStackNavigator({
-  Home: { screen: LoginSignup },
+  Home: { screen: LoginScreen },
   Map: { screen: Map },
 });
 
