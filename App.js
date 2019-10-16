d713620d57f43bb67953356c42ff4cdb6e2dfad5
Gaje Robarge <gajer@paulmitchell.edu>
2019-10-16 09:36:56 -0600

    idk

--- App.js
+++ App.js
@@ -1,47 +1,3 @@
-// import React, { Component } from 'react';
-// import { Platform, StyleSheet, Text, View } from 'react-native';
-
-// import Map from './Map';
-
-// const instructions = Platform.select({
-//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-//   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-// });
-
-
-
-// export default class App extends Component {
-//   render() {
-//     return (
-//       <Map />
-//       // <View style={styles.container}>
-//       //   <Text style={styles.welcome}>Welcome to Gaje's First React Native App</Text>
-//       //   <Text style={styles.instructions}>This hopefully will be pretty cool</Text>
-//       //   {/* <Text style={styles.instructions}>To get started, edit App.js</Text>
-//       //   <Text style={styles.instructions}>{instructions}</Text> */}
-//       // </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
 
 import React from 'react';
 import { View } from 'react-native';
@@ -64,7 +20,24 @@ export default class App extends React.Component {
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
-        />
+        >
+          <MapView.Marker
+            coordinate={{
+              latitude: 40.317521,
+              longitude: -112.004766
+            }}
+            title={"Our Home"}
+          />
+          <MapView.Marker
+            coordinate={{
+              latitude: 40.573786,
+              longitude: -111.907039
+            }}
+            title={"Paul Mitchell"}
+            description={"Work"}
+          />
+        </MapView>
+
       </View>
     );
   }
