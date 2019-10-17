2206181ef07d1066a1c503eb4d69379926b340bb
Grobarge <gajer@paulmitchell.edu>
2019-10-16 23:40:34 -0600

    finished animations and sign in page

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
+import { View, Text, StyleSheet, Dimensions, TextInput } from "react-native";
+
+import Svg, { Image, Circle, ClipPath } from 'react-native-svg';
 
 import Animated, { Easing } from 'react-native-reanimated';
 import { TapGestureHandler, State } from 'react-native-gesture-handler';
@@ -7,7 +9,7 @@ import { TapGestureHandler, State } from 'react-native-gesture-handler';
 
 const { width, height } = Dimensions.get('window');
 
-const { Value, event, block, cond, eq, set, Clock, startClock, stopClock, debug, timing, clockRunning, interpolate, Extrapolate } = Animated
+const { Value, event, block, cond, eq, set, Clock, startClock, stopClock, debug, timing, clockRunning, interpolate, Extrapolate, concat} = Animated
 
 function runTiming(clock, value, dest) {
     const state = {
@@ -50,7 +52,14 @@ class MusicApp extends Component {
             nativeEvent: ({ state }) => block([
                 cond(eq(state, State.END), set(this.buttonOpacity, runTiming(new Clock(), 1, 0)))
             ])
-        }])
+        }]);
+
+        this.onCloseState = event([{
+            nativeEvent: ({ state }) => block([
+                cond(eq(state, State.END), set(this.buttonOpacity, runTiming(new Clock(), 0, 1)))
+            ])
+        }]);
+
         this.buttonY = interpolate(this.buttonOpacity, {
             inputRange: [0, 1],
             outputRange: [100, 0],
@@ -59,18 +68,47 @@ class MusicApp extends Component {
 
         this.bgY = interpolate(this.buttonOpacity, {
             inputRange: [0, 1],
-            outputRange: [-height / 3, 0],
+            outputRange: [-height / 3 - 50, 0],
             Extrapolate: Extrapolate.CLAMP
         });
+        this.TextInputZindex = interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [1, -1],
+            Extrapolate: Extrapolate.CLAMP
+        });
+        this.TextInputY = interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [0, 100],
+            Extrapolate: Extrapolate.CLAMP
+        });
+        this.TextInputOpacity= interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [1, 0],
+            Extrapolate: Extrapolate.CLAMP
+        });
+        this.rotateCross = interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [180, 360],
+            Extrapolate: Extrapolate.CLAMP
+        });
+        
     }
     render() {
         return (
             <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'flex-end' }}>
                 <Animated.View style={{ ...StyleSheet.absoluteFill, transform: [{ translateY: this.bgY }] }}>
+                    <Svg height={height + 50} width={width} >
+                        <ClipPath id="clip" >
+                            <Circle r={height + 50} cx={width / 2} />
+                        </ClipPath>
                     <Image
-                        source={require('../assets/bg.jpg')}
-                        style={{ flex: 1, height: null, width: null }}
-                    />
+                            href={require('../assets/bg.jpg')}
+                            height={height + 50}
+                            width={width}
+                            preserveAspectRatio="xMidYMid slice"
+                            clipPath='url(#clip)'
+                        />
+                    </Svg>
                 </Animated.View>
                 <View style={{ height: height / 3, justifyContent: 'center' }}>
                     <TapGestureHandler onHandlerStateChange={this.onStateChange} >
@@ -81,6 +119,40 @@ class MusicApp extends Component {
                     <Animated.View style={{ ...styles.button, backgroundColor: '#2E71DC', opacity: this.buttonOpacity, transform: [{ translateY: this.buttonY }] }}>
                         <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'white' }} >SIGN IN WITH FACEBOOK</Text>
                     </Animated.View>
+                    <Animated.View style={{
+                        zIndex: this.TextInputZindex,
+                        opacity: this.TextInputOpacity,
+                        transform: [{ translateY: this.TextInputY }],
+                        height: height / 3, ...StyleSheet.absoluteFill,
+                        top: null,
+                        justifyContent: 'center'
+                    }}>
+                        <TapGestureHandler onHandlerStateChange={this.onCloseState} >
+                            <Animated.View style={styles.closeButton} >
+                                <Animated.Text style={{
+                                    fontSize: 15,
+                                    transform: [{ rotate: concat(this.rotateCross, 'deg')}]
+                                }}>X
+                                </Animated.Text>
+                            </Animated.View>  
+                        </TapGestureHandler>
+
+                        <TextInput
+                            placeholder="Email"
+                            style={styles.TextInput}
+                            placeholderTextColor="black"
+                        />
+                        <TextInput
+                            placeholder="Password"
+                            style={styles.TextInput}
+                            placeholderTextColor="black"
+                        />
+                        <Animated.View style={styles.button} >
+                            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Sign In</Text>
+
+                        </Animated.View>
+                    </Animated.View>
+
                 </View>
 
             </View >
@@ -106,5 +178,32 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         marginVertical: 5,
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
+    },
+    closeButton: {
+        height: 40,
+        width: 40,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        top: -20,
+        left: width / 2 - 20,
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
+    },
+
+    TextInput: {
+        height: 50,
+        borderRadius: 25,
+        borderWidth: 0.5,
+        marginHorizontal: 20,
+        paddingLeft: 20,
+        marginVertical: 5,
+        borderColor: 'rgba(0,0,0,0.2)'
     }
 })
