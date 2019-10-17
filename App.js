c72d366fa84bb24bbce04dcd6ec98740da2ff030
Grobarge <gajer@paulmitchell.edu>
2019-10-16 21:44:25 -0600

    need to get up to date

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 
 import Map from './Components/Map';
-import LoginSignup from './Components/Login-Signup';
+import Main from './Components/Main';
+
 
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 
 const MainNavigator = createStackNavigator({
-  Home: {screen: LoginSignup},
-  Map: {screen: Map},
+  Home: {screen: Main},
+  Map: { screen: Map },
 });
 
 const App = createAppContainer(MainNavigator);
