af9dd4e535356c6256eb5772db692174c0af4a3c
Gaje Robarge <gajer@paulmitchell.edu>
2019-10-18 15:37:27 -0600

    got sign in page back up, almost have functionality with google sign in

--- screens/LoginSceen.js
+++ screens/LoginSceen.js
@@ -1,49 +1,272 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import * as Expo from 'expo';
+import React from "react"
+import { StyleSheet, Text, View, Dimensions, Button, TextInput } from "react-native"
+import * as Google from 'expo-google-app-auth';
+
+import Map from './Map'
 
 
 
+import Svg, { Image, Circle, ClipPath } from 'react-native-svg';
+
+import Animated, { Easing } from 'react-native-reanimated';
+import { TapGestureHandler, State } from 'react-native-gesture-handler';
 
 
-class LoginScreen extends Component {
+const { width, height } = Dimensions.get('window');
 
-    signInWithGoogleAsync = async () => {
+const { Value, event, block, cond, eq, set, Clock, startClock, stopClock, debug, timing, clockRunning, interpolate, Extrapolate, concat } = Animated
+
+function runTiming(clock, value, dest) {
+    const state = {
+        finished: new Value(0),
+        position: new Value(0),
+        time: new Value(0),
+        frameTime: new Value(0)
+    };
+
+    const config = {
+        duration: 1000,
+        toValue: new Value(0),
+        easing: Easing.inOut(Easing.ease)
+    };
+
+    return block([
+        cond(clockRunning(clock), 0, [
+            set(state.finished, 0),
+            set(state.time, 0),
+            set(state.position, value),
+            set(state.frameTime, 0),
+            set(config.toValue, dest),
+            startClock(clock)
+        ]),
+        timing(clock, state, config),
+        cond(state.finished, debug('stop clock', stopClock(clock))),
+        state.position
+    ]);
+
+}
+
+export default class LoginScreen extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            signedIn: false,
+            name: "",
+            photoUrl: ""
+        }
+        this.buttonOpacity = new Value(1)
+
+        this.onStateChange = Animated.event([{
+            nativeEvent: ({ state }) => block([
+                cond(eq(state, State.END), set(this.buttonOpacity, runTiming(new Clock(), 1, 0)))
+            ])
+        }]);
+
+        this.onCloseState = event([{
+            nativeEvent: ({ state }) => block([
+                cond(eq(state, State.END), set(this.buttonOpacity, runTiming(new Clock(), 0, 1)))
+            ])
+        }]);
+
+        this.buttonY = interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [100, 0],
+            Extrapolate: Extrapolate.CLAMP
+        });
+
+        this.bgY = interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [-height / 3 - 50, 0],
+            Extrapolate: Extrapolate.CLAMP
+        });
+        this.TextInputZindex = interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [1, -1],
+            Extrapolate: Extrapolate.CLAMP
+        });
+        this.TextInputY = interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [0, 100],
+            Extrapolate: Extrapolate.CLAMP
+        });
+        this.TextInputOpacity = interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [1, 0],
+            Extrapolate: Extrapolate.CLAMP
+        });
+        this.rotateCross = interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [180, 360],
+            Extrapolate: Extrapolate.CLAMP
+        });
+    }
+    signIn = async () => {
         try {
-            const result = await google.logInAsync({
-                behavior: 'web',
-                iosClientId: '320038307665 - vhdh3lovsq9b9com69n155fj2k3m3ji6.apps.googleusercontent.com',
-                scopes: ['profile', 'email'],
-            });
+            const result = await Google.logInAsync({
+                iosClientId: "320038307665-vhdh3lovsq9b9com69n155fj2k3m3ji6.apps.googleusercontent.com",
+                scopes: ["profile", "email"]
+            })
 
-            if (result.type === 'success') {
-                return result.accessToken;
+            if (result.type === "success") {
+                this.setState({
+                    signedIn: true,
+                    name: result.user.name,
+                    photoUrl: result.user.photoUrl
+                })
             } else {
-                return { cancelled: true };
+                console.log("cancelled")
             }
         } catch (e) {
-            return { error: true };
+            console.log("error", e)
         }
     }
-
     render() {
         return (
-            <View style={styles.conatiner}>
-                <Button
-                    title="Sign in With Google"
-                    onPress={() => this.signInWithGoogleAsync()} />
+            <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'flex-end' }}>
+                {/* {this.state.signedIn ? (
+                    <Map />
+                ) : (
+                        <LoginPage signIn={this.signIn} />
+                    )} */}
+                {/* <View>
+                    <Text style={styles.header}>Sign In With Google</Text>
+                    <Button title="Sign in with Google" onPress={() => props.signIn()} />
+                </View> */}
+                <Animated.View style={{ ...StyleSheet.absoluteFill, transform: [{ translateY: this.bgY }] }}>
+                    <Svg height={height + 50} width={width} >
+                        <ClipPath id="clip" >
+                            <Circle r={height + 50} cx={width / 2} />
+                        </ClipPath>
+                        <Image
+                            href={require('../assets/bg.jpg')}
+                            height={height + 50}
+                            width={width}
+                            preserveAspectRatio="xMidYMid slice"
+                            clipPath='url(#clip)'
+                        />
+                    </Svg>
+                </Animated.View>
+                <View style={{ height: height / 3, justifyContent: 'center' }}>
+                    <TapGestureHandler onHandlerStateChange={this.onStateChange} >
+                        <Animated.View style={{ ...styles.button, opacity: this.buttonOpacity, transform: [{ translateY: this.buttonY }] }}>
+                            <Text style={{ fontSize: 20, fontWeight: 'bold', }} >SIGN IN</Text>
+                        </Animated.View>
+                    </TapGestureHandler>
+                    <Animated.View style={{ ...styles.button, backgroundColor: '#2E71DC', opacity: this.buttonOpacity, transform: [{ translateY: this.buttonY }] }}>
+                        <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'white' }} >SIGN IN WITH FACEBOOK</Text>
+                    </Animated.View>
+                    <Animated.View style={{
+                        zIndex: this.TextInputZindex,
+                        opacity: this.TextInputOpacity,
+                        transform: [{ translateY: this.TextInputY }],
+                        height: height / 3, ...StyleSheet.absoluteFill,
+                        top: null,
+                        justifyContent: 'center'
+                    }}>
+                        <TapGestureHandler onHandlerStateChange={this.onCloseState} >
+                            <Animated.View style={styles.closeButton} >
+                                <Animated.Text style={{
+                                    fontSize: 15,
+                                    transform: [{ rotate: concat(this.rotateCross, 'deg') }]
+                                }}>X
+                                </Animated.Text>
+                            </Animated.View>
+                        </TapGestureHandler>
+
+                        <TextInput
+                            placeholder="Email"
+                            style={styles.TextInput}
+                            placeholderTextColor="black"
+                        />
+                        <TextInput
+                            placeholder="Password"
+                            style={styles.TextInput}
+                            placeholderTextColor="black"
+                        />
+                        <Animated.View style={styles.button} >
+                            <Button style={{ fontSize: 20, fontWeight: 'bold' }} title="Sign in with Google" onPress={() => props.signIn()} ></Button>
+
+                        </Animated.View>
+                    </Animated.View>
+
+                </View>
             </View>
-        );
+        )
     }
 }
 
+// const LoginPage = props => {
+//     return (
+//         <View>
+//             <Text style={styles.header}>Sign In With Google</Text>
+//             <Button title="Sign in with Google" onPress={() => props.signIn()} />
+//         </View>
+//     )
+// }
 
-export default LoginScreen;
+// const LoggedInPage = props => {
+//     return (
+//         <View style={styles.container}>
+//             <Text style={styles.header}>Welcome:{props.name}</Text>
+//             <Image style={styles.image} source={{ uri: props.photoUrl }} />
+//         </View>
+//     )
+// }
 
 const styles = StyleSheet.create({
-    conatiner: {
+    container: {
         flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    header: {
+        fontSize: 25
+    },
+    // image: {
+    //     marginTop: 15,
+    //     width: 150,
+    //     height: 150,
+    //     borderColor: "rgba(0,0,0,0.2)",
+    //     borderWidth: 3,
+    //     borderRadius: 150
+    // }, 
+
+    button: {
+        backgroundColor: 'white',
+        height: 70,
+        marginHorizontal: 20,
+        borderRadius: 35,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        marginVertical: 5,
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
+    },
+    closeButton: {
+        height: 40,
+        width: 40,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        top: -20,
+        left: width / 2 - 20,
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
+    },
+
+    TextInput: {
+        height: 50,
+        borderRadius: 25,
+        borderWidth: 0.5,
+        marginHorizontal: 20,
+        paddingLeft: 20,
+        marginVertical: 5,
+        borderColor: 'rgba(0,0,0,0.2)'
     }
-});
+})
+
